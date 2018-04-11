6555a484fa260bb8daeb3f0769ec708de4943d1b
lucas <milucas20@hotmail.com>
2018-04-11 16:36:03 +0200

    Server-id generated auomatically. Master root password in mysql command for security

--- 5.7/replication-entrypoint.sh
+++ 5.7/replication-entrypoint.sh
@@ -9,23 +9,24 @@ relay-log=mysql-relay
 #skip-name-resolve
 EOF
 
-if [[ -z $MASTER_ROOT_PASSWORD && -z $SERVER_ID ]];then
+if [[ -z $MASTER_ROOT_PASSWORD ]];then
 
-    echo MASTER_ROOT_PASSWORD and SERVER_ID are mandatory.
+    echo MASTER_ROOT_PASSWORD is mandatory.
 
 else
-    
-echo EHHHHHHHHH server id = $SERVER_ID
 
-# If there is a linked master use linked container information
-if [ -n "$MASTER_PORT_3306_TCP_ADDR" ]; then
-  export MASTER_HOST=$MASTER_PORT_3306_TCP_ADDR
-  export MASTER_PORT=$MASTER_PORT_3306_TCP_PORT
-fi
+    $SERVER_ID = mysql -uroot -pmysql.666! -e "SHOW SLAVE HOSTS;" | sed 's/^.* \(".*"$\)/\1/' | awk 'NR>1 {print $1}' | awk 'BEGIN{a=   0}{if ($1>0+a) a=$1} END{print a+1}'
 
-if [ -z "$MASTER_HOST" ]; then
-  export SERVER_ID=1
-  cat >/docker-entrypoint-initdb.d/init-master.sh  <<'EOF'
+    echo EHHHHHHHHH222222222 server id = "$SERVER_ID" and master root password = "$MASTER_ROOT_PASSWORD"
+    # If there is a linked master use linked container information
+    if [ -n "$MASTER_PORT_3306_TCP_ADDR" ]; then
+      export MASTER_HOST=$MASTER_PORT_3306_TCP_ADDR
+      export MASTER_PORT=$MASTER_PORT_3306_TCP_PORT
+    fi
+
+    if [ -z "$MASTER_HOST" ]; then
+      export SERVER_ID=1
+      cat >/docker-entrypoint-initdb.d/init-master.sh  <<'EOF'
 #!/bin/bash
 
 echo Creating replication user ...
@@ -45,24 +46,24 @@ mysql -u root -e "\
 "
 EOF
 
-else
-  # TODO: make server-id discoverable
-  export SERVER_ID=$SERVER_ID
-  cp -v /init-slave.sh /docker-entrypoint-initdb.d/
-  cat > /etc/mysql/mysql.conf.d/repl-slave.cnf << EOF
+    else
+        # TODO: make server-id discoverable
+        export SERVER_ID=$SERVER_ID
+        cp -v /init-slave.sh /docker-entrypoint-initdb.d/
+        cat > /etc/mysql/mysql.conf.d/repl-slave.cnf << EOF
 [mysqld]
 log-slave-updates
 master-info-repository=TABLE
 relay-log-info-repository=TABLE
 relay-log-recovery=1
 EOF
-fi
+    fi
 
-cat > /etc/mysql/mysql.conf.d/server-id.cnf << EOF
+    cat > /etc/mysql/mysql.conf.d/server-id.cnf << EOF
 [mysqld]
 server-id=$SERVER_ID
 EOF
 
-exec docker-entrypoint.sh "$@"
+    exec docker-entrypoint.sh "$@"
 
 fi
